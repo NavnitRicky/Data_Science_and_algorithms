ab62eaab4f88d7413edf2e9a63f3bf24fcb6a52d
Navnit kumar <navnitricky23@gmail.com>
2025-10-07 17:09:43 +0530

    3 rd problem of day -1 placement series

---
+++ solid_Rhombus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    printf("we are going to print solid rhombus:\n");
+    for(int i=1 ;i<=4;i++){
+        for(int j=1;j<=4-i;j++){
+            printf(" ");
+        }
+             for(int j=1;j<=4;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+}
