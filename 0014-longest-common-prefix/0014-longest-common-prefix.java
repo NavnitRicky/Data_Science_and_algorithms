21a8abd5e1627437f0e1414a51da48e6e101a94c
Navnit kumar <2300090359@kluniversity.in>
2026-09-23 12:26:26 +0530

    Time: 3 ms (13.61%), Space: 43.4 MB (32.4%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,27 +1,47 @@
-class Solution {
-    public String common(String s1,String s2){
-                StringBuilder sb=new StringBuilder("");
-                int n=Math.min(s1.length(),s2.length());
-                for(int i=0;i<n;i++){
-              if(s1.charAt(i)==(s2.charAt(i))){
-                sb.append(s1.charAt(i));
-              }
-
-                else{
-                    break;
-                }
-                }
-                return sb.toString();
+// class Solution {
+//     public String common(String s1,String s2){
+//                 StringBuilder sb=new StringBuilder("");
+//                 int n=Math.min(s1.length(),s2.length());
+//                 for(int i=0;i<n;i++){
+//               if(s1.charAt(i)==(s2.charAt(i))){
+//                 sb.append(s1.charAt(i));
+//               }
+//                 else{
+//                     break;
+//                 }
+//                 }
+//                 return sb.toString();
         
-    }
+//     }
+//     public String longestCommonPrefix(String[] strs) {
+//         String res=strs[0];
+//         for(int i=1;i<strs.length;i++){
+//             res=common(res,strs[i]);
+         
+//                 }
+        
+// return res;
+        
+//     }
+// }
+class Solution {
     public String longestCommonPrefix(String[] strs) {
         String res=strs[0];
         for(int i=1;i<strs.length;i++){
-            res=common(res,strs[i]);
-         
+                    StringBuilder sb=new StringBuilder("");
+
+            for(int j=0;j<Math.min(strs[i].length(),res.length());j++){
+                if(res.charAt(j)==(strs[i].charAt(j))){
+                    sb.append(strs[i].charAt(j));
                 }
-        
-return res;
+                else{
+                    break;
+                }
+            }
+            res=sb.toString();
+        }
+        return res;
+
         
     }
 }
