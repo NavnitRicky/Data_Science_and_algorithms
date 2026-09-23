d408b4655da06935d6e03031f69684adace35b51
Navnit kumar <2300090359@kluniversity.in>
2026-09-23 15:14:55 +0530

    Time: 17 ms (40.72%), Space: 43.8 MB (73.7%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,37 +1,37 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
-        HashMap<Character,Character> map=new HashMap<>();
-                HashMap<Character,Character> map1=new HashMap<>();
+        HashMap<Character,Character> map1=new HashMap<>();
+                HashMap<Character,Character> map2=new HashMap<>();
 
-         if(s.length()!=t.length()){
+        if(s.length()!=t.length()){ //if length is not equal to the ohter string which is given
             return false;
-         }
-            for(int i=0;i<s.length();i++){
-                char ch=s.charAt(i);
-                 char ch1=t.charAt(i);
+        }
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);    //e
+            char ch1=t.charAt(i);   //t
 
-                if(map.containsKey(ch)){
-                    if(map.get(ch)!=ch1){
-                        return false;
+            if(map1.containsKey(ch)){
+                if(map1.get(ch)!=ch1){
+                    return false;
                 }
-                }
-                else{
-                    map.put(ch,ch1);
-                }
-            
-                if(map1.containsKey(ch1)){
-                    if(map1.get(ch1)!=ch){
-                        return false;
-                }
-                }
-                else{
-                    map1.put(ch1,ch);
-                }
-            
-         }
-         return true;
+            }
+            else{
+                map1.put(ch,ch1);
+            }
 
+            if(map2.containsKey(ch1)){
 
+                if(map2.get(ch1)!=ch){
+                    return false;
+                }
+            }
+            else{
+                map2.put(ch1,ch);
+            }
+
+        }
+        return true;
+
+        
     }
 }
